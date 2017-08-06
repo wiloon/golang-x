b05477ae6d6dc9f0e72a9b5f1165853ea0a8ffac
wiloon <wiloon.wy@gmail.com>
2017-08-06 17:01:38 +0800

    import bug fix

--- zookeeperx/zookeeperx_test.go
+++ zookeeperx/zookeeperx_test.go
@@ -8,18 +8,14 @@ func TestDelete0(t *testing.T) {
 	Delete(ROOT_PATH)
 }
 
-
 func TestImport(t *testing.T) {
 	ImportFromFile()
 }
-
 
 func TestExport(t *testing.T) {
 	Export(ROOT_PATH)
 
 }
-
-
 
 func TestDelete1(t *testing.T) {
 	Delete("/k0/k01")
